36be2ef4fc70533f3b65752ac8603a1e059ea21a
Sam Varghese <svarghese@walmartlabs.com>
2014-04-13 22:19:50 -0700

    Committing the files

--- db/migrate/20140414031638_create_join_table_play_list_user.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTablePlayListUser < ActiveRecord::Migration
-  def change
-    create_join_table :play_lists, :users do |t|
-      t.index [:play_list_id, :user_id]
-      # t.index [:user_id, :play_list_id]
-    end
-  end
-end
